8ab31733f66b094434b95188f0ef0b1d0f0e945e
Rahul Bajaj <63620041+rahul0eth@users.noreply.github.com>
2024-02-27 11:35:10 -0700

    comments updated

--- Day 4/nftfromscratch2.sol
+++ Day 4/nftfromscratch2.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// used to mint an NFT for buidl-portfolio-main (day 4) testing
+
 contract MyFirstNFT{
     //owner mapping
     mapping(uint => address) owners;
